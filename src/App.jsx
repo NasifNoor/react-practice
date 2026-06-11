12668426580b543192ac56389fa1aa1e0fe410b9
NasifNoor <nasifhasannoor@gmail.com.com>
2026-06-11 22:40:49 +0600

    handled pagination

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
   const [user, setUser] = useState([]);
+  const [userToShow, setUserToShow] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const fetchData = async () => {
     fetch("https://dummyjson.com/users?limit=500")
@@ -16,7 +18,26 @@ function App() {
     fetchData();
   }, []);
 
-  return !user.length ? (
+  useEffect(() => {
+    setUserToShow(user.slice(startIndex, endIndex));
+  }, [currentPage, user]);
+
+  const PAGE_SIZE = 10;
+  const totalPages = Math.ceil(user.length / PAGE_SIZE);
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+
+  const handlePrevious = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => {
+      return prev + 1;
+    });
+  };
+
+  return !userToShow.length ? (
     <> No users to show </>
   ) : (
     <>
@@ -30,7 +51,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {user?.map((item) => (
+          {userToShow?.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
               <td>
@@ -42,6 +63,17 @@ function App() {
           ))}
         </tbody>
       </table>
+      <div>
+        <button disabled={currentPage === 1} onClick={handlePrevious}>
+          Prev
+        </button>
+        <span> page {currentPage} </span>
+        <span> of </span>
+        <span>{totalPages} </span>
+        <button disabled={currentPage === totalPages} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </>
   );
 }
